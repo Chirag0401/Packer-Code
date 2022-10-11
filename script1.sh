2a61c4a373842c252738b3040c8352453e7690c2
Aadesh Bhardwaj <50766023+AadeshBhardwaj@users.noreply.github.com>
2022-10-11 19:01:42 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -6,10 +6,11 @@ sudo apt install php-fpm php-mysql -y
 sudo apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
 sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-sudo mkdir /var/www/wordpress/
-sudo cd /tmp
+cd /var/www/
 sudo curl -LO https://wordpress.org/latest.tar.gz
 sudo tar xzvf latest.tar.gz
-sudo cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
-sudo cp -a /tmp/wordpress/. /var/www/wordpress
+cd /var/www/wordpress
+sudo cp wp-config-sample.php wp-config.php
+cd ..
 sudo chown -R www-data:www-data /var/www/wordpress
+sudo rm /var/www/latest.tar.gz
